9bfa6de70ee416481fcca52753356bc9e0a3bc9d
Thane Joss <thane.joss@gmail.com>
2023-12-29 22:57:47 +0800

    modified:   script.js

--- script.js
+++ script.js
@@ -1,91 +1,73 @@
 function showImage(src, clickedThumbnail) {
-  var mainImg = document.getElementById("main-img");
-  var thumbnails = document
-    .getElementById("thumbnails")
-    .getElementsByTagName("img");
-
-  // Loop through the thumbnail images and remove the 'selected-thumbnail' class
-  for (var i = 0; i < thumbnails.length; i++) {
-    thumbnails[i].classList.remove("selected-thumbnail");
+    const mainImg = document.getElementById("main-img");
+    const thumbnails = document.querySelectorAll("#thumbnails img");
+  
+    thumbnails.forEach(thumbnail => thumbnail.classList.remove('selected-thumbnail'));
+    clickedThumbnail.classList.add('selected-thumbnail');
+  
+    mainImg.style.opacity = '0';
+  
+    const loadingImage = new Image();
+    loadingImage.onload = () => {
+      mainImg.src = loadingImage.src;
+      mainImg.style.transition = 'opacity 0.5s';
+      mainImg.style.opacity = '1';
+    };
+    loadingImage.src = buildImagePath(src);
+  
+    const downloadLink = document.getElementById('download-link');
+    downloadLink.href = buildImagePath(src);
+    downloadLink.download = src;
   }
-
-  // Add the 'selected-thumbnail' class to the clicked thumbnail
-  clickedThumbnail.classList.add("selected-thumbnail");
-
-  // Fade out the main image before changing the source
-  mainImg.style.opacity = "0";
-
-  // Load the new image
-  var loadingImage = new Image();
-  loadingImage.onload = function () {
-    // When the image is loaded, update the main image's src and fade it in
-    mainImg.src = this.src;
-    mainImg.style.opacity = "1";
-  };
-
-  // Set the source of the preloading image to the clicked thumbnail's source
-  loadingImage.src = "images/" + src;
-
-  // 更新下载链接
-  var downloadLink = document.getElementById("download-link");
-  downloadLink.href = "images/" + src; // 修改这里以匹配你的图片文件路径
-  downloadLink.download = src; // 修改这里以匹配你的图片文件路径
-}
-
-function addThumbnail(dateStr) {
-  var thumbnailsContainer = document.getElementById("thumbnails");
-  var container = document.createElement("div");
-  container.className = "thumbnail-container";
-
-  var img = document.createElement("img");
-  img.src = "images/" + dateStr + ".jpg";
-  img.alt = "Image " + dateStr;
-  img.onclick = function () {
-    showImage(dateStr + ".jpg", img);
-  };
-
-  var dateLabel = document.createElement("div");
-  dateLabel.className = "thumbnail-date";
-  dateLabel.textContent = dateStr;
-
-  container.appendChild(img);
-  container.appendChild(dateLabel);
-  thumbnailsContainer.appendChild(container);
-}
-
-function generateDateThumbnails() {
-  var endDate = new Date();
-  var startDate = new Date(2023, 11, 1);
-  var currentDate = endDate;
-
-  currentDate.setDate(currentDate.getDate() - 1);
-
-  // 清空当前的缩略图
-  document.getElementById("thumbnails").innerHTML = "";
-
-  while (currentDate >= startDate) {
-    var dateStr = formatDate(currentDate);
-    addThumbnail(dateStr);
-    // 设置为前一天
+  
+  function addThumbnail(dateStr) {
+    const thumbnailsContainer = document.getElementById("thumbnails");
+    const container = document.createElement("div");
+    container.className = "thumbnail-container";
+  
+    const img = document.createElement("img");
+    img.src = buildImagePath(dateStr + ".jpg");
+    img.alt = "Image " + dateStr;
+    img.onclick = () => showImage(dateStr + ".jpg", img);
+  
+    const dateLabel = document.createElement("div");
+    dateLabel.className = "thumbnail-date";
+    dateLabel.textContent = dateStr;
+  
+    container.append(img, dateLabel);
+    thumbnailsContainer.appendChild(container);
+  }
+  
+  function buildImagePath(filename) {
+    return "images/" + filename;
+  }
+  
+  function generateDateThumbnails() {
+    const endDate = new Date();
+    const startDate = new Date(2023, 11, 1);
+    let currentDate = new Date(endDate.getTime());
+  
     currentDate.setDate(currentDate.getDate() - 1);
+    document.getElementById("thumbnails").innerHTML = "";
+  
+    while (currentDate >= startDate) {
+      const dateStr = formatDate(currentDate);
+      addThumbnail(dateStr);
+      currentDate.setDate(currentDate.getDate() - 1);
+    }
   }
-}
-
-function formatDate(date) {
-  var year = date.getFullYear();
-  var month = ("0" + (date.getMonth() + 1)).slice(-2);
-  var day = ("0" + date.getDate()).slice(-2);
-  return "" + year + month + day;
-}
-
-// 初始化画廊并生成缩略图
-window.onload = function () {
-  generateDateThumbnails();
-  var firstThumbnail = document
-    .getElementById("thumbnails")
-    .getElementsByTagName("img")[0];
-  if (firstThumbnail) {
-    // Directly use the filename obtained from firstThumbnail.src
-    showImage(firstThumbnail.src.split("/").pop(), firstThumbnail);
+  
+  function formatDate(date) {
+    const year = date.getFullYear();
+    const month = ("0" + (date.getMonth() + 1)).slice(-2);
+    const day = ("0" + date.getDate()).slice(-2);
+    return `${year}${month}${day}`;
   }
-};
+  
+  window.onload = () => {
+    generateDateThumbnails();
+    const firstThumbnail = document.querySelector("#thumbnails img");
+    if (firstThumbnail) {
+      showImage(firstThumbnail.src.split('/').pop(), firstThumbnail);
+    }
+  };
